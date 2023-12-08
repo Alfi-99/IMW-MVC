09d4d2411d73c328bbeca8ceeb1c318585d61319
Khoirul <MSI@ADMINISTRATOR>
2023-12-08 22:49:53 +0700

    fix confirm daftar mahasiswa -> Pengguna

---
+++ IMW-MVC/Controller/PenggunaController.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using IMW_MVC.Model.Context;
+using IMW_MVC.Model.Repository;
+using IMW_MVC.Model.Entity;
+
+namespace IMW_MVC.Controller
+{
+    public class PenggunaController
+    {
+        private PenggunaRepository _repository;
+
+        //Daftar Akun
+        public bool DaftarValidasi(string nama_pengguna)
+        {
+            bool valid = false;
+            using(DbContext context = new DbContext())
+            {
+                _repository = new PenggunaRepository(context);
+                valid = _repository.DaftarValidasi(nama_pengguna);
+            }
+
+            return valid;
+        }
+        public int Create(Pengguna user)
+        {
+            bool valid = DaftarValidasi(user.nama_pengguna);
+            int result = 0;
+            if (string.IsNullOrEmpty(user.nama_pengguna))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            if (string.IsNullOrEmpty(user.katasandi))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            if(valid == true)
+            {
+                MessageBox.Show("Akun dengan nama berikut sudah ada !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            using (DbContext context = new DbContext())
+            {
+                _repository = new PenggunaRepository(context);
+                result = _repository.Create(user);
+            }
+            if (result > 0)
+            {
+                MessageBox.Show("Data Pengguna berhasil disimpan !", "Informasi",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data Pengguna gagal disimpan !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            return result;
+        }
+        public int CreateFromAdmin(Pengguna user)
+        {
+            bool valid = DaftarValidasi(user.nama_pengguna);
+            int result = 0;
+            if (string.IsNullOrEmpty(user.nama_pengguna))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            if (string.IsNullOrEmpty(user.katasandi))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            if (valid == true)
+            {
+                MessageBox.Show("Akun dengan nama berikut sudah ada !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            using (DbContext context = new DbContext())
+            {
+                _repository = new PenggunaRepository(context);
+                result = _repository.CreateFromAdmin(user);
+            }
+            if (result > 0)
+            {
+                MessageBox.Show("Data Pengguna berhasil disimpan !", "Informasi",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data Pengguna gagal disimpan !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            return result;
+        }
+        public int UpdateFromAdmin(Pengguna user, int Pengguna_ID)
+        {
+            bool valid = DaftarValidasi(user.nama_pengguna);
+            int result = 0;
+            if (string.IsNullOrEmpty(user.nama_pengguna))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            if (string.IsNullOrEmpty(user.katasandi))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            if (valid == true)
+            {
+                MessageBox.Show("Akun dengan nama berikut sudah ada !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            using (DbContext context = new DbContext())
+            {
+                _repository = new PenggunaRepository(context);
+                result = _repository.UpdateFromAdmin(user, Pengguna_ID);
+            }
+            if (result > 0)
+            {
+                MessageBox.Show("Data Pengguna berhasil disimpan !", "Informasi",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data Pengguna gagal disimpan !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            return result;
+        }
+
+
+        //Login
+        public bool LoginValidasi(string nama_pengguna, string password)
+        {
+            bool valid = false;
+            using (DbContext context = new DbContext())
+            {
+                _repository = new PenggunaRepository(context);
+                valid = _repository.LoginValidasi(nama_pengguna, password);
+            }
+
+            return valid;
+        }
+
+        public List<Pengguna> ReadUserByName(string username)
+        {
+            List<Pengguna> list = new List<Pengguna>();
+            using (DbContext context = new DbContext())
+            {
+                _repository = new PenggunaRepository(context);
+                list = _repository.ReadUserByName(username);
+            }
+            return list;
+        }
+        public bool CheckUsername(string nama_pengguna)
+        {
+            bool valid = false;
+            using (DbContext context = new DbContext())
+            {
+                _repository = new PenggunaRepository(context);
+                valid = _repository.CheckUsername(nama_pengguna);
+            }
+
+            return valid;
+        }
+        public List<Pengguna> ReadAll()
+        {
+            List<Pengguna> list = new List<Pengguna>();
+            using (DbContext context = new DbContext())
+            {
+                _repository = new PenggunaRepository(context);
+                list = _repository.ReadAll();
+            }
+            return list;
+        }
+        public int DeleteUser(Pengguna pengguna)
+        {
+            int result = 0;
+            using (DbContext context = new DbContext())
+            {
+                _repository = new PenggunaRepository(context);
+                result = _repository.DeleteUser(pengguna);
+            }
+            return result;
+        }
+    }
+}
